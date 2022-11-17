3634a8e2f921b23de2f2c02ef215ada7f26a1869
matheusalsi <msilva@inf.ufrgs.br>
2022-11-17 14:22:02 -0300

    class nn versions

--- multinn_keras_v2.py
+++ multinn_keras_v2.py
@@ -1,4 +1,5 @@
 import pandas
+from numpy import loadtxt
 from keras.models import Sequential
 from keras.layers import Dense
 from keras.wrappers.scikit_learn import KerasClassifier
@@ -7,29 +8,23 @@ from sklearn.model_selection import cross_val_score
 from sklearn.model_selection import KFold
 from sklearn.preprocessing import LabelEncoder
 from sklearn.pipeline import Pipeline
-# load dataset
-dataframe = pandas.read_csv("dataset_labeled.csv", header=None)
-dataset = dataframe.values
-X = dataset[:,0:16].astype(float)
-Y = dataset[:,16]
-# encode class values as integers
-encoder = LabelEncoder()
-encoder.fit(Y)
-encoded_Y = encoder.transform(Y)
-# convert integers to dummy variables (i.e. one hot encoded)
-dummy_y = np_utils.to_categorical(encoded_Y)
+#load dataset
+dataset = loadtxt('dataset.csv', delimiter=',')
+#split into inpux(X) and output (y) variables
+X = dataset[:,0:16]
+Y = dataset[:,-7:]
  
 # define baseline model
 def baseline_model():
 	# create model
 	model = Sequential()
 	model.add(Dense(32, input_dim=16, activation='relu'))
-	model.add(Dense(5, activation='softmax'))
+	model.add(Dense(7, activation='softmax'))
 	# Compile model
 	model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
 	return model
  
 estimator = KerasClassifier(build_fn=baseline_model, epochs=200, batch_size=5, verbose=0)
 kfold = KFold(n_splits=10, shuffle=True)
-results = cross_val_score(estimator, X, dummy_y, cv=kfold)
+results = cross_val_score(estimator, X, Y, cv=kfold)
 print("Baseline: %.2f%% (%.2f%%)" % (results.mean()*100, results.std()*100))
