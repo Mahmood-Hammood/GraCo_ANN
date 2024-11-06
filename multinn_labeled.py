4c2860d8e6b835f6de19c56bfc939cb2aec0d566
Mahmood-Hammood <115350351+Mahmood-Hammood@users.noreply.github.com>
2024-11-06 13:24:58 +0100

    Update multinn_labeled.py

--- multinn_labeled.py
+++ multinn_labeled.py
@@ -18,7 +18,7 @@ dataframe = pandas.read_csv("dataset_labeled.csv", header=None)
 dataset = dataframe.values
 X = dataset[:,0:16].astype(float)
 Y = dataset[:,16]
-# encode class values as integers
+# encode class values (labels) as integers
 encoder = LabelEncoder()
 encoder.fit(Y)
 encoded_Y = encoder.transform(Y)
@@ -39,8 +39,9 @@ estimator = KerasClassifier(build_fn=baseline_model, epochs=200, batch_size=5, v
 #kfold = KFold(n_splits=10, shuffle=True)
 #results = cross_val_score(estimator, X, dummy_y, cv=kfold)
 #print("Baseline: %.2f%% (%.2f%%)" % (results.mean()*100, results.std()*100))
+# 33% of the data will be used for testing, and the remaining 67% will be used for training.
 X_train, X_test, Y_train, Y_test = train_test_split(X, dummy_y, test_size=0.33, random_state=seed)
 estimator.fit(X_train, Y_train)
 predictions = estimator.predict(X_test)
 print(predictions)
-print(encoder.inverse_transform(predictions))
+print(encoder.inverse_transform(predictions))  ## decodes the predictions back to their original class labels using encoder.inverse_transform()
